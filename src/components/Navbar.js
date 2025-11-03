19b965a5e2a4befcb60fafeb0067487ed3f70ff5
Emmanuel Nduhiu <nduhiuemmanuel@gmail.com>
2025-11-03 20:47:24 +0300

    updated the navbar.js file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import '../css/navbar.css'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import "../css/navbar.css";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
-        <nav>
-            <ul>
-                <li>
-                    <NavLink exact to="/home">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink exact to="/about">About Us</NavLink>  
-                </li>
-                <li>
-                    <NavLink exact to="/contact">Conatct Us</NavLink>
-                </li>
-            </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact to="/home">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/about">
+              About Us
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/contact">
+              Contact Us
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
